08da9657c4398949b8abf743a6a38cb7d4e97aa7
xRumax <167364@student.uwm.edu.pl>
2024-05-16 22:36:40 +0200

    add new modules

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -40,6 +41,7 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatCardModule,
     MatInputModule,
+    MatTableModule,
   ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
