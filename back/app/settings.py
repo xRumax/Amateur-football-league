16b81761b6034bde82c5e244a03b6bc830baae4f
xRumax <167364@student.uwm.edu.pl>
2024-06-09 13:59:26 +0200

    create alembic

--- back/app/settings.py
+++ back/app/settings.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 SECRET_KEY = "@!#$!#$^@%$&1KOI#$%^JAS!#@$!@#RDAFSDFADFA"
 TOKEN_EXPIRATION = timedelta(hours=1)
 ALGORITHM = "HS256"
