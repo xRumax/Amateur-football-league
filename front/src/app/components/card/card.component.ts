b74f70a3c8b015f0a02e93d82e2c0733192800b1
xRumax <167364@student.uwm.edu.pl>
2024-10-17 21:39:01 +0200

    Update card component

--- front/src/app/components/card/card.component.ts
+++ front/src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {
   Tournament,
   TournamentService,
@@ -7,6 +7,7 @@ import { UserService } from '../../services/user.service';
 import { TeamService } from '../../services/team.service';
 import { SessionService } from '../../services/session.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -15,12 +16,15 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class CardComponent {
   tournaments: Tournament[] = [];
+  userTeam: any = {};
+  @Input() dataType!: 'tournament' | 'match';
 
   constructor(
     private snackBar: MatSnackBar,
     private tournamentService: TournamentService,
     private userService: UserService,
     private teamService: TeamService,
+    private router: Router,
     private sessionService: SessionService
   ) {}
 
@@ -37,14 +41,34 @@ export class CardComponent {
   }
 
   async joinToTournament(tournament: Tournament): Promise<void> {
-    const userId = this.sessionService.getUserId();
-    const userTeam = await this.userService.getUserTeam(Number(userId));
+    try {
+      const userId = this.sessionService.getUserId();
+      const userHasTeam = await this.userService.userHasTeam(Number(userId));
+      if (!userHasTeam) {
+        this.snackBar.open("You already don't have a team", 'Close', {
+          duration: 5000,
+        });
+        return;
+      }
+      const userTeam = await this.userService.getUserTeam(Number(userId));
 
-    await this.tournamentService.addTeamToTournament(
-      userTeam.id,
-      tournament.id
-    );
-    this.snackBar.open(`Successfully joined`, 'Close', { duration: 5000 });
-    this.loadTournaments(); // Refresh the list of tournaments
+      await this.tournamentService.addTeamToTournament(
+        userTeam.id,
+        tournament.id
+      );
+      this.snackBar.open(`Successfully joined`, 'Close', { duration: 5000 });
+      this.loadTournaments();
+    } catch (error) {
+      console.error('Error joining tournament:', error);
+      this.snackBar.open('Error joining tournament', 'Close', {
+        duration: 5000,
+      });
+    }
+  }
+
+  onCardClicked(tournamentId: any): void {
+    if (this.dataType === 'tournament') {
+      this.router.navigate(['/tournament', tournamentId]);
+    }
   }
 }
