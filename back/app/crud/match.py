0f145f207180964e9ba3202d9a29ea1dbf53ceae
xRumax <167364@student.uwm.edu.pl>
2024-10-27 22:55:12 +0100

    new method for match crud

--- back/app/crud/match.py
+++ back/app/crud/match.py
@@ -1,10 +1,16 @@
 from sqlalchemy.orm import Session
-from app.schemas.match import MatchCreate, MatchUpdate
+from app.schemas.match import MatchCreate, MatchUpdate, Match
 from app.models import match as models
 from app.models import team as team_models
+from app.models import tournament as tournament_models
+from fastapi import HTTPException
 
 
 def create_match(db: Session, match: MatchCreate):
+    db_tournament = db.query(tournament_models.Tournament).filter(tournament_models.Tournament.id == match.tournament_id).first()
+    if not db_tournament:
+        raise HTTPException(status_code=400, detail="Tournament not found")
+    
     db_match = models.Match(**match.dict())
     db.add(db_match)
     db.commit()
@@ -39,3 +45,15 @@ def delete_match(db: Session, match_id: int):
     db.delete(db_match)
     db.commit()
     return db_match
+
+def get_match_player(db:Session, match_id:int):
+    match = db.query(Match).filter(Match.id == match_id).first()
+    if not match:
+        return None
+    team_1_players = match.team_1_id.players
+    team_2_players = match.team_2_id.players
+    return {
+        "team_1_players": team_1_players,
+        "team_2_players": team_2_players
+    }
+
